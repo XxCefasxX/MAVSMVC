1f4690aa4cafad346102d44e4dedca5f640b2414
Pedro Salazar <salazar9_5@hotmail.com>
2025-05-22 19:19:04 -0700

    prueba de crear menu

--- MAVSMVC.Infraestructure/obj/Debug/net8.0/MAVSMVC.Infrastructure.AssemblyInfo.cs
+++ MAVSMVC.Infraestructure/obj/Debug/net8.0/MAVSMVC.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAVSMVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e4ac260a33a278dbc10c12bd60408b5702bb04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e038a51145c1925ba0f54c423a4e906c235fc95")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAVSMVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAVSMVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
